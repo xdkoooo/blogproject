35c8208a22bd5ab34dfd2326409b2b52b97dcc71
xdkoooo <421280548@qq.com>
2017-09-30 18:08:15 +0800

    archivesView add, categoryView add

--- blog/views.py
+++ blog/views.py
@@ -7,12 +7,7 @@ from django.views.generic import ListView
 
 # def index(request):
 #     post_list = Post.objects.all()
-#     return render(request, 'blog/index.html', context={'post_list': post_list})
-
-class IndexView(ListView):
-    model = Post
-    template_name = 'blog/index.html'
-    context_object_name = 'post_list'        
+#     return render(request, 'blog/index.html', context={'post_list': post_list})     
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -34,12 +29,28 @@ def detail(request, pk):
     }
     return render(request, 'blog/detail.html', context=context)
 
-def archives(request, year, month):
-    post_list = Post.objects.filter(created_time__year=year,
-                                    created_time__month=month).order_by('-created_time')
-    return render(request, 'blog/index.html', context={'post_list': post_list})
+# def archives(request, year, month):
+#     post_list = Post.objects.filter(created_time__year=year,
+#                                     created_time__month=month).order_by('-created_time')
+#     return render(request, 'blog/index.html', context={'post_list': post_list})
 
-def category(request, pk):
-    cate = get_object_or_404(Category, pk=pk)
-    post_list = Post.objects.filter(category=cate).order_by('-created_time')
-    return render(request, 'blog/index.html', context={'post_list': post_list})
+# def category(request, pk):
+#     cate = get_object_or_404(Category, pk=pk)
+#     post_list = Post.objects.filter(category=cate).order_by('-created_time')
+#     return render(request, 'blog/index.html', context={'post_list': post_list})
+
+class IndexView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+
+class CategoryView(IndexView):
+    def get_queryset(self):
+        cate = get_object_or_404(Category, pk=self.kwargs.get('pk'))
+        return super(CategoryView, self).get_queryset().filter(category=cate)
+
+class ArchivesView(IndexView):
+    def get_queryset(self):
+        year = self.kwargs.get('year')
+        month = self.kwargs.get('month')
+        return super(ArchivesView, self).get_queryset().filter(created_time__year=year, created_time__month=month)
