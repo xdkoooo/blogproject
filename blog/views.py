09b0669bcd87274dcab5dd053f5a51e35b4c6f2f
xdkoooo <421280548@qq.com>
2017-10-20 17:50:10 +0800

    PostDetailView add

--- blog/views.py
+++ blog/views.py
@@ -3,31 +3,31 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Category
 from comments.forms import CommentForm
 
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 # def index(request):
 #     post_list = Post.objects.all()
 #     return render(request, 'blog/index.html', context={'post_list': post_list})     
 
-def detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+# def detail(request, pk):
+#     post = get_object_or_404(Post, pk=pk)
 	
-    post.increase_views()
+#     post.increase_views()
     
-    post.body = markdown.markdown(post.body, extensions=[
-        'markdown.extensions.extra',
-        'markdown.extensions.codehilite',
-        'markdown.extensions.toc',
-        ])
+#     post.body = markdown.markdown(post.body, extensions=[
+#         'markdown.extensions.extra',
+#         'markdown.extensions.codehilite',
+#         'markdown.extensions.toc',
+#         ])
 
-    form = CommentForm()
-    comment_list = post.comment_set.all()
-    context = {
-        'post': post,
-        'form': form,
-        'comment_list': comment_list
-    }
-    return render(request, 'blog/detail.html', context=context)
+#     form = CommentForm()
+#     comment_list = post.comment_set.all()
+#     context = {
+#         'post': post,
+#         'form': form,
+#         'comment_list': comment_list
+#     }
+#     return render(request, 'blog/detail.html', context=context)
 
 # def archives(request, year, month):
 #     post_list = Post.objects.filter(created_time__year=year,
@@ -54,3 +54,36 @@ class ArchivesView(IndexView):
         year = self.kwargs.get('year')
         month = self.kwargs.get('month')
         return super(ArchivesView, self).get_queryset().filter(created_time__year=year, created_time__month=month)
+
+class PostDetailView(DetailView):
+    """docstring for PostDetailView"""
+    model = Post
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+
+    def get(self, request, *args, **kwargs):
+        response = super(PostDetailView, self).get(request, *args, **kwargs)
+        self.object.increase_views()
+
+        return response
+
+    def get_object(self, queryset=None):
+        post = super(PostDetailView, self).get_object(queryset=None)
+        post.body = markdown.markdown(post.body, 
+            extensions=[
+                        'markdown.extensions.extra',
+                        'markdown.extensions.codehilite',
+                        'markdown.extensions.toc',
+                                    ])
+        return post
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetailView, self).get_context_data(**kwargs)
+        form = CommentForm()
+        comment_list = self.object.comment_set.all()
+        context.update({
+            'form': form,
+            'comment_list': comment_list
+            })
+        return context
+        
