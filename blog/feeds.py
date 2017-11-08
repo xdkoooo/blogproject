29107168ff23b99c28cbb04977f8fafa9978f5c1
xdkoooo <421280548@qq.com>
2017-11-08 14:18:51 +0800

    rss add

---
+++ blog/feeds.py
@@ -0,0 +1,16 @@
+from django.contrib.syndication.views import Feed
+from .models import Post
+
+class AllPostsRssFeed(Feed):
+	title = "Django 博客"
+	link = "/"
+	description = "Django 博客测试文章"
+
+	def items(self):
+		return Post.objects.all()
+
+	def item_title(self, item):
+		return '[%s] %s' % (item.category, item.title)
+
+	def item_description(self, item):
+		return item.body
--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from . import views
+from blog.feeds import AllPostsRssFeed
 
 app_name = 'blog'
 urlpatterns = [
@@ -13,4 +14,5 @@ urlpatterns = [
     url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', views.ArchivesView.as_view(), name='archives'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.PostDetailView.as_view(), name='detail'),
     url(r'^tag/(?P<pk>[0-9]+)/$', views.TagView.as_view(), name='tag'),
+    url(r'^all/rss/$', AllPostsRssFeed(), name='rss'),
 ]
