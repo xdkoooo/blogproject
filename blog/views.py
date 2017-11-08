fd9c7ef84ec008dced2531297703b75b9571d4e1
xdkoooo <421280548@qq.com>
2017-11-08 17:31:44 +0800

    markdown fix

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from comments.forms import CommentForm
 
 from django.views.generic import ListView, DetailView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from markdown.extensions.toc import TocExtension
 
 # def index(request):
 #     post_list = Post.objects.all()
@@ -141,14 +142,24 @@ class PostDetailView(DetailView):
         return response
 
     def get_object(self, queryset=None):
-        post = super(PostDetailView, self).get_object(queryset=None)
-        post.body = markdown.markdown(post.body, 
-            extensions=[
-                        'markdown.extensions.extra',
-                        'markdown.extensions.codehilite',
-                        'markdown.extensions.toc',
-                                    ])
+        md = markdown.Markdown(extensions=[
+                'markdown.extensions.extra',
+                'markdown.extensions.codehilite',
+                # 'markdown.extensions.toc',
+                TocExtension(slugify=slugify),
+            ])
+        post.body = md.convert(post.body)
+        post.toc = md.toc
         return post
+
+        # post = super(PostDetailView, self).get_object(queryset=None)
+        # post.body = markdown.markdown(post.body, 
+        #     extensions=[
+        #                 'markdown.extensions.extra',
+        #                 'markdown.extensions.codehilite',
+        #                 'markdown.extensions.toc',
+        #                             ])
+        # return post
 
     def get_context_data(self, **kwargs):
         context = super(PostDetailView, self).get_context_data(**kwargs)
