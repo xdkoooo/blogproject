d593750c94535f2970d3bf5ddf6362a379e6f53a
xdkoooo <421280548@qq.com>
2017-11-08 18:15:50 +0800

    simply search funtion

--- blog/urls.py
+++ blog/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', views.ArchivesView.as_view(), name='archives'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.PostDetailView.as_view(), name='detail'),
     url(r'^tag/(?P<pk>[0-9]+)/$', views.TagView.as_view(), name='tag'),
+    url(r'^search/$', views.search, name='search'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,7 @@ from django.views.generic import ListView, DetailView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.utils.text import slugify
 from markdown.extensions.toc import TocExtension
+from django.db.models import Q
 
 # def index(request):
 #     post_list = Post.objects.all()
@@ -41,6 +42,18 @@ from markdown.extensions.toc import TocExtension
 #     cate = get_object_or_404(Category, pk=pk)
 #     post_list = Post.objects.filter(category=cate).order_by('-created_time')
 #     return render(request, 'blog/index.html', context={'post_list': post_list})
+
+def search(request):
+    q = request.GET.get('q')
+    error_msg = ''
+
+    if not q:
+        error_msg = "请输入关键词"
+        return render(request, 'blog/index.html', {'error_msg': error_msg})
+
+    post_list = Post.objects.filter(Q(title__icontains=q) | Q(body__icontains=q))
+    return render(request, 'blog/index.html', {'error_msg': error_msg, 'post_list': post_list})
+    
 
 class IndexView(ListView):
     model = Post
