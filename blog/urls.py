2340f1da827247a282710f528949bb2bb9135098
xdkoooo <421280548@qq.com>
2017-11-08 14:26:23 +0800

    rss bug fix

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import url
 from . import views
-from blog.feeds import AllPostsRssFeed
 
 app_name = 'blog'
 urlpatterns = [
@@ -14,5 +13,4 @@ urlpatterns = [
     url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', views.ArchivesView.as_view(), name='archives'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.PostDetailView.as_view(), name='detail'),
     url(r'^tag/(?P<pk>[0-9]+)/$', views.TagView.as_view(), name='tag'),
-    url(r'^all/rss/$', AllPostsRssFeed(), name='rss'),
 ]
--- blogproject/urls.py
+++ blogproject/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import url, include
+from blog.feeds import AllPostsRssFeed
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
     url(r'', include('comments.urls')),
+    url(r'^all/rss/$', AllPostsRssFeed(), name='rss'),
 ]
